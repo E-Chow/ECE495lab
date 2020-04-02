1bb008bbd0a4294681165592a11411f0010d00b5
E-Chow <62715219+E-Chow@users.noreply.github.com>
2020-04-02 14:34:40 -0400

    Update bsr_unit.vhd

--- Lab6/part2/bsr_unit.vhd
+++ Lab6/part2/bsr_unit.vhd
@@ -2,14 +2,33 @@ ibrary IEEE;
 use IEEE.std_logic_1164.all;
 
 entity bsr_unit is
-port(clk, left_right: in std_logic; 
+	
+port(bsr_clk, left_right: in std_logic; 
 	right_in, left_in: in std_logic;
-	q: out std_logic);
+	bsr_q: out std_logic);
 end bsr_unit;
 	
+architecture structure of bsr_unit is
 	
+	component mux_2to1 is
+		Port ( sel,a,b : in  STD_LOGIC;
+         		x   : out STD_LOGIC);
+	end mux_2to1;
+
+	component d_ff is
+		port( d, clk: in std_logic;
+		     q: out std_logic);
+	end d_ff;
 	
+	signal tmp: STD_LOGIC;
 	
+	begin
+		
+	Mux: mux_2to1 port map(a=>left_in, b=>right_in, sel=>let_right, x=>tmp);
+	ff: d_ff port map(d=>x, clk=>bsr_clk, q=>bsr_q);
+		
+end structure;
+		
 
 
 
