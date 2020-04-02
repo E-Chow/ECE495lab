3ec6e6a96df53fd7c5e999bec9423ab52cc5d7c1
am339 <62715476+am339@users.noreply.github.com>
2020-04-02 17:09:36 -0400

    Update bsr.vhd

--- Lab6/part2/bsr.vhd
+++ Lab6/part2/bsr.vhd
@@ -1,9 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 entity bsr is 
-	generic(n: positive);
+	generic(n: positive :=3);
 	port(shift_in: in std_logic;
 		clk, left_right: in std_logic;
 		q: out std_logic_vector(n-1 downto 0));
-endbsr;
+end bsr;
 
 architecture dataflow of bsr is
 	component bsr_unit is
@@ -12,12 +15,17 @@ architecture dataflow of bsr is
 	     bsr_q: out std_logic);
 	end component;
 	
-	signal direction => std_logic := left_right;
+	signal direction : std_logic := left_right;
+	signal tmp: std_logic_vector(n-1 downto 0);
 
 begin
-	zbit: bsr_unit port map( right_in=>q(1), left_in=> shift_in, bsr_q=>q(0), left_right=>direction, bsr_clk=>clk);
-	reg: for i is 1 to n-2 generate
-		rbit: bsr_unit port map( right_in=>q(i+1), left_in=> q(i-1), bsr_q=>q(i), left_right=>direction, bsr_clk=>clk);
+	zbit: bsr_unit port map( right_in=>tmp(1), left_in=> shift_in, bsr_q=>tmp(0), left_right=>direction, bsr_clk=>clk);
+	
+	reg: for i in 1 to n-2 generate
+		rbit: bsr_unit port map( right_in=>tmp(i+1), left_in=> tmp(i-1), bsr_q=>tmp(i), left_right=>direction, bsr_clk=>clk);
 	end generate;
-	nbit: bsr_unit port map( right_in=>shift_in, left_in=> q(n-2), bsr_q=>q(n-1), left_right=>direction, bsr_clk=>clk);
+	
+	nbit: bsr_unit port map( right_in=>shift_in, left_in=> tmp(n-2), bsr_q=>tmp(n-1), left_right=>direction, bsr_clk=>clk);
+	
+	q<=tmp;
 end dataflow;
