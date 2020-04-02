3d4dea8d17129f07e96432b4e21899dfdb249176
E-Chow <62715219+E-Chow@users.noreply.github.com>
2020-04-02 14:19:44 -0400

    Update d_ff.vhd

--- Lab6/part2/d_ff.vhd
+++ Lab6/part2/d_ff.vhd
@@ -5,15 +5,17 @@ use ieee. std_logic_unsigned.all;
  
 entity d_ff is
 port( d,clk: in std_logic;
-q: out std_logic);
+     q: out std_logic);
 end D_FF;
  
 architecture behavioral of d_ff is
 begin
+ 
 process(d,clk)
-begin
-ifrising_edge(clk) then
-q <= d;
-end if;
+begin 
+     if rising_edge(clk) then
+          q <= d;
+     end if;
 end process;
+      
 end behavioral;
