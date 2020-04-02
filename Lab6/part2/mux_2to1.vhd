b60b73ad7d70809d97d528c9224c5462bc10de61
E-Chow <62715219+E-Chow@users.noreply.github.com>
2020-04-02 14:25:16 -0400

    Update mux_2to1.vhd

--- Lab6/part2/mux_2to1.vhd
+++ Lab6/part2/mux_2to1.vhd
@@ -2,9 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity mux_2to1 is
-    Port ( SEL : in  STD_LOGIC;
-           A   : in  STD_LOGIC;
-           B   : in  STD_LOGIC;
+    Port ( SEL, A, B : in  STD_LOGIC;
            X   : out STD_LOGIC);
 end mux_2to1;
 
