6bbb862dc5d91eeab42df544007650e6f65825b9
am339 <62715476+am339@users.noreply.github.com>
2020-04-02 22:14:22 -0400

    Update and rename bsr_test8 to bsr_test8.vhd

--- Lab6/part2/bsr_test8.vhd
+++ Lab6/part2/bsr_test8.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 
 entity bsr_test8 is
 port (shift_in: in std_logic;
-clk, left_right: in std_logic;
-q: out std_logic_vector(7 downto 0)
+      clk, left_right: in std_logic;
+      q: out std_logic_vector(7 downto 0)
 );
 end bsr_test8;
 
@@ -13,8 +13,8 @@ architecture testing of bsr_test8 is
 component bsr is 
 generic (n: positive:=8);
 	Port(shift_in:in std_logic;
-				clk, left_right: in std_logic;
-				q: out std_logic_vector(n-1 downto 0));
+	     clk, left_right: in std_logic;
+	     q: out std_logic_vector(n-1 downto 0));
 end component;
 
 begin
