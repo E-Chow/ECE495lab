8b84cc398b1151417d5375d0b9d5c65fb23d382d
am339 <62715476+am339@users.noreply.github.com>
2020-04-09 13:46:27 -0400

    Update lab7.vhd

--- Lab7/lab7.vhd
+++ Lab7/lab7.vhd
@@ -5,19 +5,19 @@ use lpm.lpm_components.all;
 
 entity lab7 is
 	port (clk, clear: in std_logic;
-			useqEnOut: out std_logic;
-			ctrlSignals: out std_logic_vector(0 to 7));
+	      useqEnOut: out std_logic;
+	      ctrlSignals: out std_logic_vector(0 to 7));
 end lab7;
 
 architecture structure of lab7 is
 
 	component exp7_useq is
 		generic (uROM_width: integer;
-					uROM_file: string);
+			 uROM_file: string);
 		port (opcode: in std_logic_vector(3 downto 0);
-				uop: out std_logic_vector(1 to (uROM_width-9));
-				enable, clear: in std_logic;
-				clock: in std_logic);
+		      uop: out std_logic_vector(1 to (uROM_width-9));
+		      enable, clear: in std_logic;
+		      clock: in std_logic);
 	end component;
 					
 	signal useqEnable, useqClear: std_logic;
@@ -25,7 +25,7 @@ architecture structure of lab7 is
 	
 begin
 	delay: lpm_counter generic map(lpm_width=>2)
-							 port map(clock=>clk, cout=>useqEnable);
+			      port map(clock=>clk, cout=>useqEnable);
 	useqClear <= clear;
 	useqEnOut <= useqEnable;
 	labelG: for i in 0 to 7 generate
@@ -33,7 +33,7 @@ begin
 	end generate;
 	
 	useq: exp7_useq generic map (uROM_width=>17, uROM_file=>"uROM_file.mif")
-							 port map (uop=>uop,enable=>useqEnable,
-										  clear=>useqClear, clock=>clk,opcode=>"0011");
+			   port map (uop=>uop,enable=>useqEnable,
+				     clear=>useqClear, clock=>clk,opcode=>"0011");
 	
 end structure;
